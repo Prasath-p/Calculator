abdcf817283c01c459de797e0d2f5d421664cb39
Prasath p <96309277+Prasath-p@users.noreply.github.com>
2022-02-28 17:54:30 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Calculator from "./component/Cal";
+import "./App.css";
+
+
+function App() {
+  return(
+    <>
+    <Calculator />
+    </>
+  );
+}
+
+export default App;
+
